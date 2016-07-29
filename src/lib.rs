e3ea81123246e512fd10d64f44b41aed527da679
jadon belezos <jadon.belezos@autodesk.com>
2016-07-29 13:17:22 -0700

    made some updates to network frc library

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,49 @@
 use std::thread;
-use std::net::{Ipv4Addr,UdpSocket,TcpListener,TcpStream};
+use std::net::{Ipv4Addr, UdpSocket, TcpListener, TcpStream};
 use std::io::prelude::*;
+// const TEAM_ID : i32 = 9999; //default
+extern "C" fn start_thread(team_id: i32) {
+    execute_thread(team_id.clone());
+    println!("start_thread")
+
+}
+struct frcNetImpl {
+    lastDynamicControlPacket : [Vec<u8>; 32]
+}
+
+fn execute_thread(team_id: i32) {
+    // extract the ip of the robot
+    let ip = Ipv4Addr::new(10,(team_id/100) as u8,(team_id % 100) as u8,0);
+    let port = 1110;
+    let mut buf = [0 as u8; 1024];
+
+// receive info from driverstations
+    let mut dsServer =  UdpSocket::bind((ip,port)).unwrap();
+    dsServer.set_read_timeout(None);// always blocking
+    println!("binded dsPort");
+
+    let mut robotSocket =  UdpSocket::bind((ip,port)).unwrap();
+    robotSocket.set_read_timeout(None);// always blocking
+    println!("binded robotSocket");
+
+    while(enabled) {
+        //read a message and echo it in reverse back
+        let (sizeMsg,src) = dsServer.recv_from(&mut buf).unwrap(); 
+        if sizeMsg < 0 {println!("read failed");}
+    }
+
+
+    
+    
+}
+
+
+
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-	super::start_thread(9999);
-    }
-}
-//const TEAM_ID : i32 = 9999; //default
-extern fn start_thread(team_id : i32){
-    //thread::spawn(move ||{}); 
-execute_thread(team_id.clone());
-    println!("start_thread")
-
-}
-fn execute_thread(team_id :i32){
-// extract the ip of the robot
-   // let ip = Ipv4Addr::new(10,(team_id/100) as u8,(team_id % 100) as u8,15);
-   let ip = Ipv4Addr::new(0,0,0,0); 
-    let port = 8080;
-    let mut buf = [0 as u8; 100];
-    
-// receive info from driverstations
-//let mut dsServer =  UdpSocket::bind((ip,port)).unwrap();
-    //dsServer.set_read_timeout(None);
-    //let mut ts = TcpStream::new((ip,port));
-    let tcpL = TcpListener::bind((ip,port)).unwrap();
-    for stream  in tcpL.incoming() { //wait for incoming from putty
-        let mut conn = stream.unwrap();
-        println!("accepted connection");
-        loop {
-            let sizeMsg = conn.read(&mut buf).unwrap(); //read a message and echo it in reverse back
-            let bufRet = &mut buf[..sizeMsg];
-            bufRet.reverse();
-            conn.write(&bufRet);
-        }
+        super::start_thread(9999);
     }
 }

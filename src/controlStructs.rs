096a4a7b00a6e86bddcd12091cb6310d5a27cbab
jadon belezos <jadon.belezos@autodesk.com>
2016-08-01 15:25:44 -0700

    fix naming errors and finshing protocol logic

--- src/controlStructs.rs
+++ src/controlStructs.rs
@@ -1,12 +1,25 @@
+///this the receive packet format
 #[repr(c)]
-pub struct ControlData2015 {
+pub struct commonControlData2015 {
      packetIndex : u8,
      unknown : u8,
      state : u8,
      station : u8,
-     joysticks : [joystickData; 3]
+     joysticks : [joystickData; 4],
+    dynamicSize : u8
+}
+pub struct dynaChunk {
+    id : u8,
+    data : vec<u8>,
+}
+impl ControlData2015 {
+    pub fn generatePacket(){
+
+
+    }
 }
 
+// send packet format
 #[repr(c)]
 struct RobotControl2015 {
     packetIndex : u8,
@@ -43,8 +56,8 @@ mod implt {
       axes_size : u8,
       axis : [u8; 6], 
       unkown : u8,
-      button : u8,
-      pov_size : u8,
+      buttons : u16,
+      pov_size : u8, //unsure
       pov : [u8; 2]
     }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use std::thread;
+extern crate std;
 use std::net::{Ipv4Addr, UdpSocket, TcpListener, TcpStream};
-use std::io::prelude::*;
-use std::mem::cp;
-use std::mem::sizeOf;
+use std::mem::{sizeOf,transmute_copy,transmute};
+mod controlStructs;
+mod crc;
 /// this code is rewrite of frc network communcations
 /// library from Aardvark-Wpilib
 /// this code is written in rust programming
@@ -10,7 +10,8 @@ use std::mem::sizeOf;
 /// this library is designed to be use concepts  that can easily be translated to c++
 /// if neccessary.
 extern "C" fn start_thread(team_id: i32) {
-    execute_thread(team_id.clone());
+    let threadInfo = frcNetImpl {};
+    threadInfo.execute_thread(team_id.clone());
     println!("start_thread")
 
 }
@@ -22,48 +23,76 @@ struct frcNetImpl {
 
 }
 
-fn execute_thread(team_id: i32) {
-    let team_id = team_id; //const param.
+impl frcNetImpl {
+    fn execute_thread(team_id: i32) {
+        let team_id = team_id; //const param.
 
-    // extract the ip of the robot
-    let ip = Ipv4Addr::new(10,(team_id/100) as u8,(team_id % 100) as u8,0);
-    let port = 1110;
-    let mut buf = [0 as u8; 1024];
+        // extract the ip of the robot
+        let ip = Ipv4Addr::new(10,(team_id/100) as u8,
+                (team_id % 100) as u8,0);
+        let port = 1110;
+        let mut recvBuf = [0 as u8; 1024];
+        let mut sendBufer = [0 as u8; 2048];
 
-// receive info from driverstations
-    let mut dsServer =  UdpSocket::bind((ip,port)).unwrap();
-    dsServer.set_read_timeout(None);// always blocking
-    println!("binded dsPort");
+    // receive info from driverstations
+        let mut dsServer =  UdpSocket::bind((ip,port)).unwrap();
+        dsServer.set_read_timeout(None);// always blocking
+        println!("binded dsPort");
 
-    let mut robotSocket =  UdpSocket::bind((ip,port)).unwrap();
-    robotSocket.set_read_timeout(None);// always blocking
-    println!("binded robotSocket");
+        let mut robotSocket =  UdpSocket::bind((ip,port)).unwrap();
+        robotSocket.set_read_timeout(None);// always blocking
+        println!("binded robotSocket");
 
-    while enabled {
-       //read a message and echo it in reverse back
-       let (sizeMsg,src) = dsServer.recv_from(&mut buf).unwrap(); 
-       if (sizeMsg > 0) && (sizeMsg = std::mem::sizeOf::<commonControlData2015>()) {
-           println!("read failed or different packet format");
-       } else { 
-           //convert data to packet format. 
-           let dataPacket : commonControlData2015 =  unsafe { mem::transmute_copy(&buf)}; 
-           readDynamicData();
-           
-           let sendPacket = commonControlData2015.generate(&mut self.packetIndex);
-           addDynamicChunks(&mut dynChunks,&mut sendBuffer);
+        while enabled {
+           //read a message and echo it in reverse back
+           let (sizeMsg,src) = dsServer.recv_from(&mut recvBuf).unwrap(); 
+           if (sizeMsg < 0) 
+           && (sizeMsg == sizeOf::<controlStruct::commonControlData2015>()) {
+               println!("read failed or different packet format");
+           } else { 
+               //convert data to packet format. 
+               let dataPacket : controlStruct::commonControlData2015 = 
+                   unsafe { transmute_copy(&buf[0..sizeMsg])}; 
+               self.readDynamicData(&mut buf);
+               
+               //create packet for send buffer.
+               let sendPacket = commonControlData2015.generate(&mut self);
+               let packetSize = sizeOf::<controlStruct::commonControlData2015>();
+               sendBuf[0..packetSize].clone_from_slice(unsafe {mem::transmute(sendPacket) });
+               
 
-           generateCRC(&mut sendbuffer);
-           dsServer.send(addr, &mut sendBuffer);
+
+                let crc = generateCRC(&mut sendbuffer);
+                
+                sendBuf[packetSize..4].clone_from_slice(unsafe{mem::transmute(crc)});
+
+               dsServer.send(addr, &mut sendBuffer[0..(packetSize+4)]);
+
+            }
         }
     }
+    ///I am not sure what we do with dynamic data.
+    /// as far as I can tell  we attempt 
+    /// read the data into buffers and resend the data
+    /// I have no idea why I would send the dynamic chunks as 
+    /// is
+    fn readDynamicData(&self, buf :&mut [u8]) -> () {
+    let mut curLoc = std::mem::sizeOf::<commonControlData2015>();
+    let mut sizeOfblock = self.lastDataPacket.dynaSize;
+    for packets in self.lastDynamicPackets {
+       *packets.clear(); //empty vector
+       *packets.reserve(sizeOfBlock);
+       *packets.extend_from_slice(buf [curLoc..sizeOfBlock]);
+
+       curLoc += sizeOfBlock;
+       sizeOfBlock =  buf[curLoc];
+      }  
+    }
+
+    fn addDynamicChunks(&self) -> i32 {/*this is filler function*/ }
 
 
-    
-    
 }
-
-
-
 
 #[cfg(test)]
 mod tests {
